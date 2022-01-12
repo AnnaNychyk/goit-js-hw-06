c6a756a0bd46525e21c3f0928495e6e93c02d5b5
AnnaNychyk <annanichichka@gmail.com>
2022-01-12 11:57:07 +0200

    done task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listIngredientsEl = document.querySelector('#ingredients');
+
+const makeListOfIngredients = ingredients.map(ingredient => {
+    const ingredientEl = document.createElement('li');
+    ingredientEl.classList.add('item');
+    ingredientEl.textContent = ingredient;
+
+    return ingredientEl
+  });
+
+listIngredientsEl.append(...makeListOfIngredients)
+
