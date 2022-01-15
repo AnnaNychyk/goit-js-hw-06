441a5f65c56e425171d48b2ee065cf9f4538725e
AnnaNychyk <annanichichka@gmail.com>
2022-01-15 14:59:55 +0200

    done task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const formRef = document.querySelector('.login-form')
 
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+    // console.log(formElements);
+
+    const mail = formElements.email.value;
+    const password = formElements.password.value;
+
+    if (mail === '' || password === '') {
+        alert('Заполните все пустые поля')
+    }
+    const formData = { mail, password }
+    console.log(formData)
+    formRef.reset();
+}
+
+formRef.addEventListener('submit', onFormSubmit);
