c30acdcc9ff60c382a6773a0ae958f2d2f56befe
AnnaNychyk <annanichichka@gmail.com>
2022-01-16 09:37:11 +0200

    done task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnChangeColor = document.querySelector('.change-color');
+const textColorChange = document.querySelector('.color');
+
+function onClick() { 
+  textColorChange.textContent = getRandomHexColor();
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+btnChangeColor.addEventListener('click', onClick);
