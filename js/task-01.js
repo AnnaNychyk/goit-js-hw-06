13abe13885b9790ed441b5614826289614d24717
AnnaNychyk <annanichichka@gmail.com>
2022-03-24 09:05:56 +0200

    change task 1

--- js/task-01.js
+++ js/task-01.js
@@ -6,17 +6,6 @@ for (let i = 0; i < categoryEl.length; i += 1) {
 const titleOfCategoryEl = categoryEl[i].firstElementChild.textContent;
 console.log(`Category: ${titleOfCategoryEl}`)
 
-const elementsOfCategory = categoryEl[i].querySelectorAll('li');
-console.log(`Elements: ${elementsOfCategory.length}`)
+const elementsOfCategory = categoryEl[i].lastElementChild.querySelectorAll('li');
+console.log(`Elements: ${elementsOfCategory}`)
 }
-
-// const allCategories = document.querySelectorAll (`ul#categories li.item`);
-// const numberOfCategories = allCategories.length;
-// console.log (`Number of categories: ${numberOfCategories}`);
-// for (let i = 0; i < numberOfCategories; i += 1) {
-//     const headText = allCategories[i].firstElementChild.textContent;
-//     console.log (`Category: ${headText}`);
-// const elementsEl = allCategories[i].lastElementChild;
-// const numberOfElements = elementsEl.querySelectorAll(`li`);
-// console.log (`Elements: ${numberOfElements.length}`)
-// }
